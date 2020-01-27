50fd39458cf764e4e8bb1ddb480604fbe02f3835
sharad <sharad.fourbrick@gmail.com>
2020-01-27 19:40:03 +0530

    Update databasefile.py

--- databasefile.py
+++ databasefile.py
@@ -5,8 +5,8 @@ import commonfile
 import json
 import smtplib 
 import config
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+# from sendgrid import SendGridAPIClient
+# from sendgrid.helpers.mail import Mail
 
 def InsertQuery(table,columns,values):
     try:
