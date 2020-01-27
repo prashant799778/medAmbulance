57539e6cba40e87c5d0c55666facf348fbaf9afd
sharad <sharad.fourbrick@gmail.com>
2020-01-27 19:30:18 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -913,32 +913,6 @@ def trackRider():
 
 
 
-@app.route('/addDriver', methods=['POST'])
-def addDriver():
-    try:
-    	data1 = commonfile.DecodeInputdata(request.get_data())
-        if data !=None :
-            column="name,mobile,driverId,ambulanceModeId,ambulanceId,panCardNo,DlNo,currentLocation,currentLocationlatlong,vehicleNo"
-
-        	values="'"+str(data["name"])+"','"+str(data["mobile"])"','"+str(data["userId"])+"','"+str(data1["ambulanceModeId"])+"','"
-        	values=values+"'"+str(data1["ambulanceId"])+"','"+str(data1["panCardNo"])+"','"+str(data1["DlNo"])+"','"
-        	values="'"+str(data["currentLocation"])+"','"+str(data["currentLocationlatlong"])+"','"+str(data1["vehicleNo"])+"'"
-
-        	insertdata=databasefile.InsertQuery("driverMaster",column,values)
-           
-
-            column = " * "
-        	whereCondition= "mobile='"+str(data1["mobile"])+ "'"
-        	data17= databasefile.SelectQuery1("driverMaster",column,whereCondition)
-            
-            output= {"result":"Revert You Soon,After Verification","status":"true"}
-            return output
-        else:
-            output = {"result":"User Already Added Existed ","status":"false"}
-            return output    
-    except Exception as e :
-        print("Exception---->" + str(e))    
-        output = {"result":"something went wrong","status":"false"}
 
 
 
