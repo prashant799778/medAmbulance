ee4108343c15ba7a78a0b5096c39f1522b9f98bc
Nisha2807 <57443914+Nisha2807@users.noreply.github.com>
2020-02-11 12:10:21 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -178,7 +178,7 @@ def addDriver():
             data= databasefile.SelectQuery("userMaster",column,whereCondition)
             print(data,'data')
             
-            DlNo,DlFrontfilename,DlFrontPicPath,DlBackfilename,DlBackPicPath="","","","",""
+            DlNo,dlFrontFilename,DlFrontPicPath,dlBackFilename,DlBackPicPath="","","","",""
 
             if 'DlNo' in inputdata:
                 DlNo=inputdata["DlNo"]
