107eb2c7db0f2f3c426626cb69c252def5bc0d17
Nisha2807 <57443914+Nisha2807@users.noreply.github.com>
2020-02-11 14:00:48 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -342,23 +342,7 @@ def addDriver():
                         return data
                 else:
                     return commonfile.Errormessage()
-
-
-
-        #         column="name,mobile,driverId,ambulanceModeId,ambulanceId,panCardNo,DlNo,currentLocation,currentLocationlatlong,vehicleNo"
-        #         values=  "'"+str(data["name"])+"','"+str(data["mobile"])+"','"+str(data["userId"])+"','"+str(data1["ambulanceModeId"])+"','"
-        #         values=values +str(data1["ambulanceId"])+"','"+str(data1["panCardNo"])+"','"+str(data1["DlNo"])+"','"
-        #         values=values+str(data["currentLocation"])+"','"+str(data["currentLocationlatlong"])+"','"+str(data1["vehicleNo"])+"'"
-        #         insertdata=databasefile.InsertQuery("driverMaster",column,values)
-        #         column = " * "
-        #         whereCondition= "mobile='"+str(data1["mobile"])+ "'"
-        #         data17= databasefile.SelectQuery1("driverMaster",column,whereCondition)
-        #         output= {"result":"Revert You Soon,After Verification","status":"true"}
-        #         return output
-        #     else:
-        #         output = {"result":"User Already Added Existed ","status":"false"}
-        #         return output
-        # else:
+        else:
             return msg
     except Exception as e :
         print("Exception---->" + str(e))    
