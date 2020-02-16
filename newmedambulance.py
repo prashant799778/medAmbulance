04d0e3fa3e40a125156b19d61e15b81fbe1921e1
sharad <sharad.fourbrick@gmail.com>
2020-02-17 02:21:31 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -1818,7 +1818,7 @@ def getNearAmbulance():
             column=  " d.name, d.mobileNo, d.ambulanceId, a.ambulanceNo, a.lat, a.lng "
             whereCondition= " a.onTrip=0 and a.onDuty=1 and a.ambulanceId=d.ambulanceId"
             
-            orderby=" ((a.lat-"+str(userLat)+") + (a.lng-"+str(userLng)+")) limit 1"
+            orderby=" ((45.11-"+str(userLat)+") + (45.11-"+str(userLng)+")) limit 1"
             loginuser=databasefile.SelectQueryOrderbyAsc("ambulance a, driverMaster d",column,whereCondition,"",orderby,"","")
             if (loginuser!=0):   
                                
