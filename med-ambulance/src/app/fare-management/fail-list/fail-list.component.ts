2adf93feb4035ad408bcb0cf703e8070f2505994
vijay.pal <vijay.pal@fourbrick.com>
2020-02-17 19:27:02 +0530

    add image

--- med-ambulance/src/app/fare-management/fail-list/fail-list.component.ts
+++ med-ambulance/src/app/fare-management/fail-list/fail-list.component.ts
@@ -7,12 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FailListComponent implements OnInit {
 	tableHeading = [
-		"No", "Vehicle Type", "Fare Per KM", "Minimum Fare","Minimum Fare", "Minimum Distance","Waiting Fare","Action"
+		"No", "Vehicle Type", "Fare Per KM", "Minimum Fare", "Minimum Distance","Waiting Fare","Action"
 	]
 	heading='Fare List'
+	fareListData = []
 	constructor() { }
 
 	ngOnInit() {
+		this.getFareList()
+	}
+
+	getFareList(){
+		this.fareListData = [
+			{	'vehicleType':'CNG',
+				'farePerKM': 10,
+				'minFare': 100,
+				'minDistance': 5,
+				'waitingFare': 100,
+				
+			},
+			{	'vehicleType':'Petrol',
+				'farePerKM': 25,
+				'minFare': 100,
+				'minDistance': 5,
+				'waitingFare': 100,
+				
+			},
+			{	'vehicleType':'Desile',
+				'farePerKM': 20,
+				'minFare': 100,
+				'minDistance': 5,
+				'waitingFare': 100,
+				
+			}
+		]
 	}
 
 }
