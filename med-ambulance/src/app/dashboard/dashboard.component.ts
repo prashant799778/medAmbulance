a8fcf3c4dd2cede528ae80ae71fc132ad414d308
vijay.pal <vijay.pal@fourbrick.com>
2020-02-18 12:32:50 +0530

    add review

--- med-ambulance/src/app/dashboard/dashboard.component.ts
+++ med-ambulance/src/app/dashboard/dashboard.component.ts
@@ -8,13 +8,36 @@ import { AppSettings } from '../utils/constant';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  
+	reviewData = []
 	constructor(public userService: UserService) { }
 
 	ngOnInit() {
 		this.userService.getApiData(AppSettings.alldriver).then(resp=>{
 			console.log(resp)
 		})
+		this.reviewData = 	[ 
+								{
+									'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
+									'name': 'Nisha',
+									'review':'Good Serivces',
+									'email': 'nisha@gmail.com',
+									'star': 4
+								},
+								{
+									'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
+									'name': 'Vijay',
+									'review':'Good Serivces',
+									'email': 'nisha@gmail.com',
+									'star': 4
+								},
+								{
+									'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
+									'name': 'Hemant',
+									'review':'Good Serivces',
+									'email': 'nisha@gmail.com',
+									'star': 4
+								}
+							]
 	}
 
 }
