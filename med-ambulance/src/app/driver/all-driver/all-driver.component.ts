d16f70a709cd1957d77f1de796c9cbdc131fd80d
vijay.pal <vijay.pal@fourbrick.com>
2020-02-18 17:32:58 +0530

    madechanges

--- med-ambulance/src/app/driver/all-driver/all-driver.component.ts
+++ med-ambulance/src/app/driver/all-driver/all-driver.component.ts
@@ -25,32 +25,35 @@ export class AllDriverComponent implements OnInit {
 				this.driverData = resp['result']
 			}
 		})
-		// this.driverData = [
-		// 	{	'name':'Vijay Pal',
-		// 		'mobileNo': 8888517655,
-		// 		'email': 'vijay@gmail.com',
-		// 		'address': 'Noida UP-87',
-		// 		'wallet': 100,
-		// 		'tripCount': 10,
-		// 		'status': 1
-		// 	},
-		// 	{	'name':'Vijay Pal',
-		// 		'mobileNo': 8888517655,
-		// 		'email': 'vijay@gmail.com',
-		// 		'address': 'Noida UP-87',
-		// 		'wallet': 100,
-		// 		'tripCount': 10,
-		// 		'status': 1
-		// 	},
-		// 	{	'name':'Vijay Pal',
-		// 		'mobileNo': 8888517655,
-		// 		'email': 'vijay@gmail.com',
-		// 		'address': 'Noida UP-87',
-		// 		'wallet': 100,
-		// 		'tripCount': 10,
-		// 		'status': 1
-		// 	}
-		// ]
+		this.driverData = [
+			{	'name':'Vijay Pal',
+				'mobileNo': 8888517655,
+				'email': 'vijay@gmail.com',
+				'currentLocation': 'Noida UP-87',
+				'wallet': 100,
+				'dateCreate': '02/10/2015',
+				'tripCount': 10,
+				'status': 1
+			},
+			{	'name':'Vijay Pal',
+				'mobileNo': 8888517655,
+				'email': 'vijay@gmail.com',
+				'currentLocation': 'Noida UP-87',
+				'wallet': 100,
+				'dateCreate': '02/10/2015',
+				'tripCount': 10,
+				'status': 0
+			},
+			{	'name':'Vijay Pal',
+				'mobileNo': 8888517655,
+				'email': 'vijay@gmail.com',
+				'currentLocation': 'Noida UP-87',
+				'wallet': 100,
+				'dateCreate': '02/10/2015',
+				'tripCount': 10,
+				'status': 2
+			}
+		]
 	}
 
 	getStatus(status){
