84d7383e8b213b511848ada61ca5343c3639b29b
vijay.pal <vijay.pal@fourbrick.com>
2020-02-18 13:53:18 +0530

    add driver

--- med-ambulance/src/app/dashboard/dashboard.component.ts
+++ med-ambulance/src/app/dashboard/dashboard.component.ts
@@ -9,35 +9,76 @@ import { AppSettings } from '../utils/constant';
 })
 export class DashboardComponent implements OnInit {
 	reviewData = []
+	driverData = []
 	constructor(public userService: UserService) { }
 
 	ngOnInit() {
 		this.userService.getApiData(AppSettings.alldriver).then(resp=>{
 			console.log(resp)
 		})
+		this.driverData = [
+			{	'name':'Vijay Pal',
+				'mobileNo': 8888517655,
+				'email': 'vijay@gmail.com',
+				'currentLocation': 'Noida UP-87',
+				'wallet': 100,
+				'dateCreate': '02/10/2015',
+				'tripCount': 10,
+				'status': 1
+			},
+			{	'name':'Vijay Pal',
+				'mobileNo': 8888517655,
+				'email': 'vijay@gmail.com',
+				'currentLocation': 'Noida UP-87',
+				'wallet': 100,
+				'dateCreate': '02/10/2015',
+				'tripCount': 10,
+				'status': 0
+			},
+			{	'name':'Vijay Pal',
+				'mobileNo': 8888517655,
+				'email': 'vijay@gmail.com',
+				'currentLocation': 'Noida UP-87',
+				'wallet': 100,
+				'dateCreate': '02/10/2015',
+				'tripCount': 10,
+				'status': 2
+			}
+		] 
 		this.reviewData = 	[ 
-								{
-									'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
-									'name': 'Nisha',
-									'review':'Good Serivces',
-									'email': 'nisha@gmail.com',
-									'star': 4
-								},
-								{
-									'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
-									'name': 'Vijay',
-									'review':'Good Serivces',
-									'email': 'nisha@gmail.com',
-									'star': 4
-								},
-								{
-									'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
-									'name': 'Hemant',
-									'review':'Good Serivces',
-									'email': 'nisha@gmail.com',
-									'star': 4
-								}
-							]
+			{
+				'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
+				'name': 'Nisha',
+				'review':'Good Serivces',
+				'email': 'nisha@gmail.com',
+				'star': 4
+			},
+			{
+				'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
+				'name': 'Vijay',
+				'review':'Good Serivces',
+				'email': 'nisha@gmail.com',
+				'star': 4
+			},
+			{
+				'userImage': 'http://radixtouch.in/templates/templatemonster/ecab/source/assets/img/user/usrbig1.jpg',
+				'name': 'Hemant',
+				'review':'Good Serivces',
+				'email': 'nisha@gmail.com',
+				'star': 4
+			}
+		]
+	}
+
+	getStatus(status){
+		if(status == 0){
+			return 'ON TRIP'
+		}else if(status == 1){
+			return 'AVAILABLE'
+		}else if(status == 2){
+			return 'LEAVE'
+		}
+
 	}
 
 }
