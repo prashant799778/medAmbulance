9bc6ace831c033d5834cc74c7c574f0e3bce4ca1
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-18 12:00:31 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -783,7 +783,7 @@ def allAmbulance():
                 if inputdata['endlimit'] != "":
                     endlimit =str(inputdata["endlimit"])
 
-            column=" AM.ambulanceId,AM.lat,AM.lng,atm.ambulanceType,am.ambulanceType as Category,AM.transportType,AM.transportModel,AM.color,AM.ambulanceRegistrationFuel,AM.typeNo,AM.ambulanceFilename,AM.ambulanceFilepath,AM.ambulanceModeId,AM.ambulanceTypeId "
+            column=" AM.ambulanceId,AM.lat,AM.lng,atm.ambulanceType,am.ambulanceType as category,AM.transportType,AM.transportModel,AM.color,AM.ambulanceRegistrationFuel,AM.typeNo,AM.ambulanceFilename,AM.ambulanceFilepath,AM.ambulanceModeId,AM.ambulanceTypeId "
            
             whereCondition=" and  AM.ambulanceTypeId=atm.id and AM.ambulanceModeId=am.id"
             data=databasefile.SelectQuery2("ambulanceMaster as AM, ambulanceTypeMaster  as atm,ambulanceMode as am",column,whereCondition,"",startlimit,endlimit)
