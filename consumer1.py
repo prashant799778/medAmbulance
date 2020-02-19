e08d72fc3c70e9a5ffc139fafaf345085d70ff22
sharad <sharad.fourbrick@gmail.com>
2020-02-19 20:33:41 +0530

    Update consumer1.py

--- consumer1.py
+++ consumer1.py
@@ -28,24 +28,30 @@ def on_message(client, userdata, msg):
     column=" ambulanceId, onTrip,onDuty "
     whereCondition=" ambulanceId='"+str(ambulanceId)+"'"
     ambulanceTripDetails = databasefile.SelectQuery1("ambulanceRideStatus",column,whereCondition)
-    print(ambulanceTripDetails,"wqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqwwwwwwwwwwww")
+    
 
-    column1=" id,bookingId "
-    whereCondition1=" and  ambulanceId='"+str(ambulanceId)+"'"
-    orderby=" id "
-    ambulanceRideId = databasefile.SelectQueryOrderby("bookAmbulance",column1,whereCondition1,"","0","1",orderby)
-    print("=============================",ambulanceRideId)
-    if ambulanceRideId!=0:
-      if (ambulanceTripDetails[0]["onTrip"]==1) and (ambulanceTripDetails[0]["onDuty"]==1):
+    #if ambulanceRideId!=0:
+    if (ambulanceTripDetails[0]["onTrip"]==1) and (ambulanceTripDetails[0]["onDuty"]==1):
 
-        column=" rideId,ambulanceId,driverId,lat,lng "
-        values="'"+str(ambulanceRideId["result"][0]["bookingId"])+"','"+str(ambulanceId)+"','"+str(driverId)+"','"+str(lat)+"','"+str(lng)+"'"
-        insertdata=databasefile.InsertQuery("ambulanceRideTracking",column,values)
-        print("[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]",insertdata)
-    # whereCondition="ambulance_Id='"+str(ambulanceId1)+"'  and hospital_Id='"+str(mainId)+"'"
-    # column=""
-    # values="'"+str(mainId)+"','"+str(ambulanceId1)+"'"
-    # insertdata=databasefile.InsertQuery("hospitalambulanceMapping",column,values)
+      column1=" id,bookingId "
+      whereCondition1=" and  ambulanceId='"+str(ambulanceId)+"'"
+      orderby=" id "
+      ambulanceRideId = databasefile.SelectQueryOrderby("bookAmbulance",column1,whereCondition1,"","0","1",orderby)
+
+
+
+      column=" rideId,ambulanceId,driverId,lat,lng "
+      values="'"+str(ambulanceRideId["result"][0]["bookingId"])+"','"+str(ambulanceId)+"','"+str(driverId)+"','"+str(lat)+"','"+str(lng)+"'"
+      insertdata=databasefile.InsertQuery("ambulanceRideTracking",column,values)
+      
+    
+    elif  (ambulanceTripDetails[0]["onTrip"]==0) and (ambulanceTripDetails[0]["onDuty"]==1):
+      print("2222222222222222222")
+      column= " lat='" + str(lat) ++"',lng='"+ str(lng) + "'"
+      whereCondition=" and ambulanceId='" + str(ambulanceId)+   "' "
+      data = databasefile.UpdateQuery("responderMode",column,whereCondition)
+
+    
   except Exception as e :
     print("Exception---->" + str(e))    
     output = {"result":"something went wrong","status":"false"}
