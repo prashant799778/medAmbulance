4b2632fe05f1df63de4b583439f70c1fa47ccd24
sharad <sharad.fourbrick@gmail.com>
2020-02-19 20:01:50 +0530

    Update consumer1.py

--- consumer1.py
+++ consumer1.py
@@ -20,14 +20,26 @@ def on_message(client, userdata, msg):
   try:
     data = json.loads(data)
     print(data)
+    ambulanceId=data["ambulanceId"]
+    driverId=data["driverId"]
+    lat=data["lat"]
+    lng=data["lng"]
 
     column=" ambulanceId, onTrip,onDuty "
     whereCondition=" ambulanceId='"+str(data["ambulanceId"])+"'"
     ambulanceTripDetails = databasefile.SelectQuery1("ambulanceRideStatus",column,whereCondition)
-    print(ambulanceTripDetails)
-    return {"status":"ok"}
+    
 
+    column1=" id,bookingId "
+    whereCondition1=" ambulanceId='"+str(data["ambulanceId"])+"'"
+    orderby=" id "
+    ambulanceRideId = databasefile.SelectQueryOrderbyAsc("ambulanceRideStatus",column1,whereCondition1,"",orderby,"0","1")
+    
+    if (ambulanceTripDetails[0]["onTrip"]==1) and (ambulanceTripDetails[0]["onDuty"]==1):
 
+      column=" rideId,ambulanceId,driverId,lat,lng "
+      values="'"+str(ambulanceRideId["result"][0]["bookingId"])+"','"+str(ambulanceId)+"','"+str(driverId)+"','"+str(lat)+"','"+str(lng)+"'"
+      insertdata=databasefile.InsertQuery("hospitalambulanceMapping",column,values)
 
     # whereCondition="ambulance_Id='"+str(ambulanceId1)+"'  and hospital_Id='"+str(mainId)+"'"
     # column=""
