6f745598e8485c1638d39cbf7704f9fba59b2b09
vijay.pal <vijay.pal@fourbrick.com>
2020-02-19 10:54:47 +0530

    add logout fuctionality

--- med-ambulance/src/app/side-bar/side-bar.component.ts
+++ med-ambulance/src/app/side-bar/side-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthsService } from '../services/auths.service';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-side-bar',
@@ -8,13 +10,23 @@ import { Router } from '@angular/router';
 })
 export class SideBarComponent implements OnInit {
 
-	constructor(public router:Router) { }
+	constructor(public router:Router,public authsService: AuthsService) { }
 
 	ngOnInit() {
 	}
 
 	goToPage(routes){
 		this.router.navigateByUrl('/'+routes)
+	}
+	logout(){
+   
+		this.authsService.logout()
+	}
+	logouts(){
+		jQuery('#logout-pop').modal('show')
+	}
+	closeModal(){
+		jQuery('#logout-pop').modal('hide')
 	}
 
 }
