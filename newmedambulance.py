ea6d1bc2c613d2f2a36707e230f70f9a6aeb172f
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-21 14:13:14 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -2051,9 +2051,9 @@ def allHospital():
                 Id=request.args["hospitalId"]
                 whereCondition2=" and  hosp.id  = '" + str(Id) + "'  "    
 
-            column= "hosp.id,hosp.hospitalName,hosp.address,am.ambulanceType,hosp.longitude,hosp.latitude,am.id as ambulanceTypeId"   
-            WhereCondition=  " hosp.id=ahm.hospital_Id and am.id=ahm.ambulance_Id"+whereCondition+whereCondition2
-            data=databasefile.SelectQuery1("hospitalMaster as hosp,hospitalambulanceMapping as ahm,ambulanceTypeMaster as am",column,WhereCondition)
+            column= "hosp.id,hosp.hospitalName,hosp.address,am.ambulanceType,hosp.longitude,hosp.latitude,am.id as ambulanceTypeId,fm.name as facility"   
+            WhereCondition=  " hosp.id=ahm.hospital_Id and am.id=ahm.ambulance_Id and hosp.id=hFm.hospitalId and fm.id=hFm.facilityId"+whereCondition+whereCondition2
+            data=databasefile.SelectQuery1("hospitalMaster as hosp,hospitalambulanceMapping as ahm,ambulanceTypeMaster as am,facilityMaster as fm,hospitalFacilityMapping as hFm",column,WhereCondition)
             print(data,'data')
             if (data!=0): 
                 print(data)
