55f159e3896f930a5880ea5755850c2afdbe72d1
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-21 17:00:09 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -2103,7 +2103,7 @@ def allHospital1():
                 whereCondition2=" and  hosp.id  = '" + str(Id) + "'  "    
 
             column= "hosp.id,hosp.hospitalName,hl.address,hl.lat,hl.lng"   
-            WhereCondition=  " hl.hospitalId=hosp.id"
+            WhereCondition=  " hl.hospitalId=hosp.id"+whereCondition2
             data=databasefile.SelectQuery1("hospitalMaster as hosp,hospitalLocationMaster as hl",column,WhereCondition)
             if (data!=0): 
                 a=[]
