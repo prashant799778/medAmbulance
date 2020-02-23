a6a2a3f3425ea29a172c7f11d134cfb4e0922343
sharad <sharad.fourbrick@gmail.com>
2020-02-23 15:37:21 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -2587,6 +2587,7 @@ def bookRide():
         keyarr = ["ambulanceId","id",'startLocationLat','startLocationLong',"pickupLocationAddress",'dropLocationLat','dropLocationLong',"dropLocationAddress","userId"]
         for i in inputdata["driverId"]: 
             topic=str(i)+"/booking"
+            print("=================",topic)
             client.publish(topic, str(inputdata1))
             print("2222222222222") 
         return  {"result":"booking send","status":"True"}
