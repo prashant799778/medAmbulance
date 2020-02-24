b4254c509c9bbf1d68498692bdeff9b26a30d2d6
sharad <sharad.fourbrick@gmail.com>
2020-02-24 19:42:00 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -7,7 +7,7 @@ import time
 def on_connect(client, userdata, flags, rc):
 	print("-------Connected-------",rc)
 	client.subscribe("#")
-	client.publish("topic1","data111111")
+	#client.publish("topic1","data111111")
 	
 
 #def on_message(client, userdata, msg):    
