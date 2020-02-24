86ff0b6d65ea965fb579fa5e3131d786f4469ca5
sharad <sharad.fourbrick@gmail.com>
2020-02-24 17:44:43 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = 'secret!'
 client = mqtt.Client()
 client.connect("localhost",1883,60)
 while True:
-    client.publish("outTopic", '{"userId":"91dbe288564e11ea93d39ebd4d0189fc","ambulanceId":1,"RideId":"cca056e84f1311ea93d39ebd4d0189fc","driverId":1,"lat":28.456789,"lng":77.072472}')
+    client.publish("outTopic", '{"userId":"91dbe288564e11ea93d39ebd4d0189fc","ambulanceId":1,"RideId":"cca056e84f1311ea93d39ebd4d0189fc","driverId":1,"lat":28.456788,"lng":77.072472}')
  
 # if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5054, debug=True)
