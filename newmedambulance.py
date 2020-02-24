8e42b66020645e9e04d6cf431e210ca4d88b0a54
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-24 14:00:25 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -2263,7 +2263,8 @@ def allHospital1():
             column= "hosp.id,hosp.hospitalName,hl.address,hl.lat,hl.lng,cm.name as city,cm.id as cityId"   
             WhereCondition=  " and  hl.hospitalId=hosp.id and hosp.status<>'2' and hl.cityId=cm.id "+whereCondition2+whereCondition3+whereCondition4
             data=databasefile.SelectQuery2("hospitalMaster as hosp,hospitalLocationMaster as hl,cityMaster as cm",column,WhereCondition,"",startlimit,endlimit)
-            countdata=databasefile.SelectQuery1('hospitalMaster as hosp,hospitalLocationMaster as hl,cityMaster as cm',column,WhereCondition) 
+            countdata=databasefile.SelectQuery1('hospitalMaster as hosp,hospitalLocationMaster as hl,cityMaster as cm',column,WhereCondition)
+            print(countdata) 
             if (data!=0): 
                 a=[]
                 b=[]
