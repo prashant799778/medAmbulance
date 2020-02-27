fd8d5c673e658def351a8b55dd8efe7cdca35d47
prashant <prashantgoyal.fourbrick@gmail.com>
2020-02-27 17:48:47 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -3376,7 +3376,7 @@ def ActiveTrip():
                 if inputdata['endLimit'] != "":
                     endlimit =str(inputdata["endLimit"])
 
-            whereCondition=" and  bm.status=1  and bm.userMobile=um.mobileNo and bm.driverId=dm.id "
+            whereCondition=" and  bm.status=1  and bm.userMobile=um.mobileNo and bm.driverId=dm.driverId "
 
             column="bm.userMobile,bm.bookingId,bm.pickup as tripFrom,bm.dropOff as tripTo,date_format(bm.ateCreate,'%Y-%m-%d %H:%i:%s')startTime,dm.name as driverName,um.name as userName"
             data=databasefile.SelectQuery2("bookAmbulance as bm,userMaster as um,driverMaster as dm",column,whereCondition,"",startlimit,endlimit)
