09ff261b241f3de8cee20d7ae1cc720f06722197
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-30 14:32:57 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -7356,12 +7356,12 @@ def responderTrip():
 
 
 
-            whereCondition="  and  bm.userId=um.userId and bm.driverId=dm.driverId and dm.driverId='"+str(userId)+"' "+whereCondition2
+            whereCondition=" and bm.userId=um.userId and bm.driverId=dm.driverId and a.driverId=dm.driverId and a.ambulanceId=b.ambulanceId  and dm.driverId='"+str(userId)+"' "+whereCondition2
 
             column="bm.id,bm.userMobile,bm.driverMobile,bm.bookingId,bm.pickup as tripFrom,bm.dropOff as tripTo,date_format(bm.dateCreate,'%Y-%m-%d %H:%i:%s')startTime,dm.name as driverName,um.name as userName,bm.status,bm.finalAmount,bm.totalDistance,b.lng,SQRT(POW(69.1 * (b.lat - bm.pickupLatitude, 2) +POW(69.1 * (bm.pickupLongitude - b.lng) * COS(b.lat / 57.3), 2)) AS distancefromCustomer"
-            data=databasefile.SelectQueryOrderby("bookResponder as bm,userMaster as um,driverMaster as dm,ambulanceRideStatus as b ",column,whereCondition,"",startlimit,endlimit,orderby)
+            data=databasefile.SelectQueryOrderby("bookResponder as bm,userMaster as um,driverMaster as dm, ambulanceRideStatus as b,ambulanceMaster as a",column,whereCondition,"",startlimit,endlimit,orderby)
             print(data,"--------------------------------------------------")
-            countdata=databasefile.SelectQuery4("bookResponder as bm,userMaster as um,driverMaster as dm,ambulanceRideStatus as b",column,whereCondition)
+            countdata=databasefile.SelectQuery4("bookResponder as bm,userMaster as um,driverMaster as dm,ambulanceRideStatus as b,ambulanceMaster as a",column,whereCondition)
            
             if (data['status']!='false'):
                 for i in data['result']:
