2db7c96b2f131c8295c571ecbf6dc5912d11c809
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-02 20:26:13 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -5751,9 +5751,10 @@ def updateStatus():
                 whereCondition= " userId = '" + str(driverId) + "'"
                 deviceKey=databasefile.SelectQuery("userMaster",column,whereCondition)
                 print(deviceKey,"++++++++++=")
-                deviceKey=deviceKey["result"]["deviceKey"]
+                # deviceKey=deviceKey["result"]["deviceKey"]
                 email=deviceKey['result']['email']
                 name=deviceKey['result']['name']
+                
 
                 message = Mail(
                     from_email = 'adbcd@medbulance.com',
