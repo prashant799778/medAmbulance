af6f5965f602b4143ac9e938c4dee57e60a429b6
prashant <prashantgoyal.fourbrick@gmail.com>
2020-04-04 11:16:29 +0530

    Update newmedambulance.py

--- newmedambulance.py
+++ newmedambulance.py
@@ -7473,14 +7473,14 @@ def support():
 
             whereCondition=" and bm.ambulanceId=am.ambulanceId  and am.ambulanceModeId=aM.Id and   cq.id=bm.questionId and  am.ambulanceTypeId=atm.id  and  bm.userId=um.userId and bm.driverId=dm.driverId and bm.status='3' and um.userId='"+str(userId)+"' "+whereCondition2
 
-            column="bm.id,bm.userMobile,bm.drivermobile,bm.bookingId,cq.questions,atm.ambulanceType as ambulanceTypeId ,aM.ambulanceType as ambulanceTypeId,bm.pickup as tripFrom,bm.dropOff as tripTo,date_format(bm.ateCreate,'%Y-%m-%d %H:%i:%s')startTime,dm.name as driverName,um.name as userName,bm.status,bm.finalAmount,bm.totalDistance,bm.canceledUserId"
+            column="bm.id,bm.userMobile,bm.drivermobile,bm.userId,bm.driverId,bm.bookingId,cq.questions,atm.ambulanceType as ambulanceTypeId ,aM.ambulanceType as ambulanceTypeId,bm.pickup as tripFrom,bm.dropOff as tripTo,date_format(bm.ateCreate,'%Y-%m-%d %H:%i:%s')startTime,dm.name as driverName,um.name as userName,bm.status,bm.finalAmount,bm.totalDistance,bm.canceledUserId"
             data=databasefile.SelectQueryOrderby("bookAmbulance as bm,userMaster as um,driverMaster as dm,ambulanceMaster as am,ambulanceTypeMaster as atm,ambulanceMode as aM, cancelquestions  as cq",column,whereCondition,"",startlimit,endlimit,orderby)
             print(data,"--------------------------------------------------")
             # countdata=databasefile.SelectQuery4("bookAmbulance as bm,userMaster as um,driverMaster as dm",column,whereCondition)
 
             whereCondition3=" and bm.ambulanceId= am.ambulanceId and cq.id=bm.questionId and  am.ambulanceModeId=aM.Id and bm.status='3' and   bm.userId=um.userId and bm.driverId=dm.driverId and um.userId='"+str(userId)+"' "+whereCondition2
 
-            column2="bm.id,bm.userMobile,bm.drivermobile,bm.bookingId,cq.questions,aM.ambulanceType as ambulanceTypeId,bm.pickup as tripFrom,bm.dropOff as tripTo,date_format(bm.dateCreate,'%Y-%m-%d %H:%i:%s')startTime,dm.name as driverName,um.name as userName,bm.status,bm.finalAmount,bm.totalDistance,bm.canceledUserId"
+            column2="bm.id,bm.userMobile,bm.drivermobile,bm.bookingId,bm.userId,bm.driverId,cq.questions,aM.ambulanceType as ambulanceTypeId,bm.pickup as tripFrom,bm.dropOff as tripTo,date_format(bm.dateCreate,'%Y-%m-%d %H:%i:%s')startTime,dm.name as driverName,um.name as userName,bm.status,bm.finalAmount,bm.totalDistance,bm.canceledUserId"
             data2=databasefile.SelectQueryOrderby("bookResponder as bm,userMaster as um,driverMaster as dm,ambulanceMaster as am,ambulanceTypeMaster as atm,ambulanceMode as aM, cancelquestions  as cq",column2,whereCondition3,"",startlimit,endlimit,orderby)
             print(data,"--------------------------------------------------")
             # countdata2=databasefile.SelectQuery4("bookResponder as bm,userMaster as um,driverMaster as dm,ambulanceMaster as am,ambulanceMode as aM",column2,whereCondition3)
